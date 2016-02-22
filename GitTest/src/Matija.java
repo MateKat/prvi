9c65f522d2387541ec34fd0256b7a996e972e737
MateKat <skoric001@net.hr>
2016-02-22 23:08:53 +0100

    b2

--- GitTest/src/Matija.java
+++ GitTest/src/Matija.java
@@ -3,5 +3,9 @@ public class Matija {
 
 	public static  void matija(){
 		System.out.println("Bok");
+		System.out.println("Bok");
+		System.out.println("Bok");
+		System.out.println("Bok");
+
 	}
 }

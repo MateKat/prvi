7288c94081a649fd95e8375938ddb0a0c62297a8
MateKat <skoric001@net.hr>
2016-02-24 00:27:49 +0100

    Commit aloha

--- GitTest/src/Matija.java
+++ GitTest/src/Matija.java
@@ -1,11 +1,12 @@
 
 public class Matija {
+	private boolean test = true;
 
 	public static  void matija(){
 		System.out.println("Bok");
 		System.out.println("Bok");
 		System.out.println("Bok");
 		System.out.println("Bok");
-
+		
 	}
 }
